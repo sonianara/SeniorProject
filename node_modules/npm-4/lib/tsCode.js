6ae87511d8fc54656122860dc6cf5535659a2701
sonianara <sonaraya@calpoly.edu>
2018-02-10 17:37:55 -0800

    created react-native structure

---
+++ node_modules/npm-4/lib/tsCode.js
@@ -0,0 +1,10 @@
+"use strict";
+var Greeter = (function () {
+    function Greeter() {
+    }
+    Greeter.prototype.greet = function (name) {
+        return "A type-scripting greeting to you, " + name;
+    };
+    return Greeter;
+}());
+module.exports = Greeter;
