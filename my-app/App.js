5fed239b09b31248acaedf656dbda028777bb4aa
sonianara <sonaraya@calpoly.edu>
2018-02-17 15:28:12 -0800

    started working on messages

--- my-app/App.js
+++ my-app/App.js
@@ -4,12 +4,14 @@ import Button from './components/Button/Button.js';
 import MenuBar from './components/NavigationBar/MenuBar.js';
 import AccountSettings from './screens/Settings/AccountSettings.js';
 import ProfileSettings from './screens/Settings/ProfileSettings.js';
+import MessageList from './components/Message/MessageList.js';
 
 export default class App extends React.Component {
   render() {
     return (
+	<MessageList></MessageList>
       //<View style={styles.container}>
-	<ProfileSettings></ProfileSettings>
+	//<ProfileSettings></ProfileSettings>
         //<Text>Open up App.js to start working on your app!</Text>
         //<Text>Changes you make will automatically reload.</Text>
         //<Text>Shake your phone to open the developer menu.</Text>
