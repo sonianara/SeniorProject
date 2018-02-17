5f24b41a85f22b8c57b62e4ef51949f6586b69ef
sonianara <sonaraya@calpoly.edu>
2018-02-17 12:49:21 -0800

    Added button

---
+++ my-app/components/Button/Button.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Button from 'react-native-button';
+
+export default class ButtonComponent extends Component {
+   constructor(props, context) {
+      super(props, context);
+   }
+
+   _handlePress() {
+      console.log('Pressed!');
+   }
+
+
+   render() {
+      return (
+         <Button 
+            style = {{fontSize:20, color: 'green'}}
+ 	    styleDisabled={{color: 'red'}}
+            onPress={() => this._handlePress()}>
+	    Press Me!
+	 </Button>
+      );
+   }
+};
+
+
