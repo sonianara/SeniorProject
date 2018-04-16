f9822a0b514f55c3ba5fb3e7dd7acdde27861c05
sonianara <sonaraya@calpoly.edu>
2018-04-16 15:46:48 -0700

    updates

--- my-app/screens/Settings/ProfileSettings.js
+++ my-app/screens/Settings/ProfileSettings.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Alert} from 'react-native';
 import SettingsList from 'react-native-settings-list';
+import EditSettingsComponent from 'EditSettingsComponent';
 
 export default class ProfileSettingsComponent extends Component {
 
@@ -9,6 +10,12 @@ export default class ProfileSettingsComponent extends Component {
       this.onValueChange = this.onValueChange.bind(this);
       this.state = {switchValue: false};
    }
+
+   switchToEdit(pageHeader, field) {
+     const {state, navigate} = this.props.navigation;
+     navigate('EditSettingsComponent', {go_back_key: state.key, user: userInfo});
+   }
+
    render() {
       var bgColor = '#DCE3F4';
       return (
@@ -17,21 +24,16 @@ export default class ProfileSettingsComponent extends Component {
 	       <Text style={{alignSelf:'center', marginTop:50, marginBottom:10, fontWeight:'bold', fontSize: 16}}>Profile Settings</Text>
 	    </View>
 	    <View style={{backgroundColor:'#EFEFF4', flex:1}}>
-               <SettingsList borderColor='#c8c7cc' defaultItemSize={50}>
-                     <SettingsList.Header headerStyle={{marginTop:15}}/>
+            <SettingsList borderColor='#c8c7cc' defaultItemSize={50}>
+              <SettingsList.Header headerStyle={{marginTop:15}}/>
 	          <SettingsList.Item titleInfo = '22' hasNavArrow={true} title='Age'
-			switchState={this.state.switchValue} switchOnValueChange={this.onValueChange}
-			onPress={() => Alert.alert('Clicked Age')}/>
-	          <SettingsList.Item titleInfo = 'Male' hasNavArrow={true} title='Gender'
-			switchState={this.state.switchValue} switchOnValueChange={this.onValueChange}/>
-		  <SettingsList.Item titleInfo = 'Male, Female' hasNavArrow={true} title='Interested In (Gender)'
-			switchState={this.state.switchValue} switchOnValueChange={this.onValueChange} />
-      <SettingsList.Item titleInfo = '20-25' hasNavArrow={true} title='Interested In (Age)'
-			switchState={this.state.switchValue} switchOnValueChange={this.onValueChange} />
-		  <SettingsList.Item titleInfo = '25 miles' hasNavArrow={true} title='Distance'
-			switchState={this.state.switchValue} switchOnValueChange={this.onValueChange}/>
-		  <SettingsList.Item title = 'Filtered Words'
-			onPress={() => Alert.alert('Routing to FilteringWords page')}/>
+			         onPress={() => this.switchToEdit('Age', '22')/>
+	          <SettingsList.Item titleInfo = 'Male' hasNavArrow={true} title='Gender'/>
+      		  <SettingsList.Item titleInfo = 'Male, Female' hasNavArrow={true} title='Interested In (Gender)'/>
+            <SettingsList.Item titleInfo = '20-25' hasNavArrow={true} title='Interested In (Age)'/>
+      		  <SettingsList.Item titleInfo = '25 miles' hasNavArrow={true} title='Distance'/>
+      		  <SettingsList.Item title = 'Filtered Words'
+			         onPress={() => Alert.alert('Routing to FilteringWords page')}/>
                </SettingsList>
             </View>
         </View>
