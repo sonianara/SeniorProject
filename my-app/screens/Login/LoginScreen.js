9e06a521bf1de06b494096d36104230fbf98e4a8
sonianara <sonaraya@calpoly.edu>
2018-04-16 14:18:57 -0700

    adding user to firebase db

--- my-app/screens/Login/LoginScreen.js
+++ my-app/screens/Login/LoginScreen.js
@@ -3,28 +3,53 @@ import { StyleSheet, Text, View, Image, TextInput, Alert, TouchableHighlight } f
 import facebookLogo from '../../resources/loginWithFacebook.png';
 import appLogo from '../../resources/appLogo.png';
 import Button from 'react-native-button';
-
+import * as firebase from 'firebase';
+import config from '../../App.js';
 import ExploreScreen from '../../screens/Profile/ProfileScreen.js';
 
 const APP_ID = "413413412439784";
 
 export default class LoginScreen extends React.Component {
-   constructor(props) {
-      super(props);
-   }
+
+  userExists = async (user) => {
+    var db = firebase.database();
+    return await db.ref('users').child('users/user ' + user.id).once('value', function(snapshot) {
+      var exists = snapshot.val() !== null
+      return exists;
+    });
+  }
+
+  addUserToDatabase = (user) => {
+    firebase.database().ref('users/user ' + user.id).update({
+      "name": user.name,
+      "birthday":user.birthday,
+      "hometown":user.hometown,
+    });
+  }
 
    logIn = async () => {
-     const { state, navigate } = this.props.navigation;
-     const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(APP_ID, {
-         permissions: ['public_profile', 'user_photos'],
-       });
-     if (type === 'success') {
-       const fields = 'name,picture';
-       const response = await fetch(`https://graph.facebook.com/me?access_token=${token}&fields=${fields}`);
-       const userInfo = await response.json();
-       navigate('ProfileScreen', { go_back_key: state.key, user: userInfo });
-     }
-   }
+      const { state, navigate } = this.props.navigation;
+      const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
+        APP_ID,
+        { permissions: ['public_profile', 'user_photos', 'user_birthday', 'user_hometown'] }
+      );
+      if (type === 'success') {
+        const credential = firebase.auth.FacebookAuthProvider.credential(token);
+
+        firebase.auth().signInWithCredential(credential).catch((error) => {
+          // Handle Errors here.
+        });
+        const fields = 'name,picture.width(200).height(200),birthday,hometown';
+        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}&fields=${fields}`);
+        const userInfo = await response.json();
+
+        if (!this.userExists(userInfo)) {
+          this.addUserToDatabase(userInfo);
+        }
+        this.addUserToDatabase(userInfo);
+        navigate('ProfileScreen', { go_back_key: state.key, user: userInfo });
+      }
+    }
 
    render() {
       return (
