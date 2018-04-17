4648a176e51a81930b2e864135dfa9b8f0bd1b06
Alexandra <adeany@calpoly.edu>
2018-04-16 17:01:43 -0700

    basic layout in place for editing settings info

--- my-app/screens/Settings/EditSettings.js
+++ my-app/screens/Settings/EditSettings.js
@@ -1,34 +1,51 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, Alert, TextInput} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import Prompt from 'rn-prompt';
 import SettingsList from 'react-native-settings-list';
+import { Header } from 'react-native-elements';
 
 export default class EditSettingsComponent extends Component {
-  constructor() {
-     super();
-     this.onValueChange = this.onValueChange.bind(this);
-     this.state = {switchValue: false};
+  constructor(props) {
+    super(props);
+    const { state, navigate } = this.props.navigation;
+    this.state = { inputText: state.params.fieldValue, promptVisible: false };
+  }
+
+  handleEdit = (newValue) => {
+    this.setState({ inputText: newValue, promptVisible: false });
+    // TODO: Update value in database
+    // TODO: Update value on previous profile settings page
   }
 
   render() {
-     var bgColor = '#DCE3F4';
-     return (
-        <View style={{backgroundColor:'#EFEFF4', flex:1}}>
-           <View style={{borderBottomWidth:1, backgroundColor: '#f7f7f8', borderColor:'#c8c7cc'}}>
-          <Text style={{alignSelf:'center', marginTop:50, marginBottom:10, fontWeight:'bold', fontSize: 16}}>Profile Settings</Text>
+    var bgColor = '#DCE3F4';
+
+    const { state, navigate } = this.props.navigation;
+    const pageHeader = state.params.pageHeader;
+    const field = state.params.fieldValue;
+
+    return (
+      <View style={{ backgroundColor: '#EFEFF4', flex: 1 }}>
+        <View style={{ borderBottomWidth: 1, backgroundColor: '#f7f7f8', borderColor: '#c8c7cc' }}>
+          <Text style={{ alignSelf: 'center', marginTop: 50, marginBottom: 10, fontWeight: 'bold', fontSize: 16 }}>
+            Edit {pageHeader}
+          </Text>
         </View>
-        <View style={{backgroundColor:'#EFEFF4', flex:1}}>
-            <SettingsList borderColor='#c8c7cc' defaultItemSize={50}>
-                    <SettingsList.Header headerStyle={{marginTop:15}}/>
-            <SettingsList.Item titleInfo = '22' hasNavArrow={true} title='Age'
-            switchState={this.state.switchValue} switchOnValueChange={this.onValueChange}
-            onPress={() => Alert.alert('Clicked Age')}/>
-
-           </View>
-       </View>
-     );
-  }
-
-  onValueChange(value) {
-     this.setState({switchValue: value});
+        <View style={{ backgroundColor: '#EFEFF4', flex: 1 }}>
+          <SettingsList borderColor='#c8c7cc' defaultItemSize={50}>
+            <SettingsList.Header headerStyle={{ marginTop: 15 }} />
+            <SettingsList.Item titleInfo={this.state.inputText} hasNavArrow={false} title={pageHeader}
+              onPress={() => this.setState({ promptVisible: true })} />
+          </SettingsList>
+        </View>
+        <Prompt
+          title={"Enter New Value for " + pageHeader + ":"}
+          placeholder={field}
+          defaultValue={field}
+          visible={this.state.promptVisible}
+          onCancel={() => this.setState({ promptVisible: false })}
+          onSubmit={(value) => this.handleEdit(value)} />
+      </View>
+    );
   }
 }
--- my-app/screens/Settings/ProfileSettings.js
+++ my-app/screens/Settings/ProfileSettings.js
@@ -1,46 +1,50 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, Alert} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import SettingsList from 'react-native-settings-list';
-import EditSettingsComponent from 'EditSettingsComponent';
+import EditSettingsComponent from '../Settings/EditSettings';
 
 export default class ProfileSettingsComponent extends Component {
 
-   constructor() {
-      super();
-      this.onValueChange = this.onValueChange.bind(this);
-      this.state = {switchValue: false};
-   }
+  constructor() {
+    super();
+    this.onValueChange = this.onValueChange.bind(this);
+    this.state = { switchValue: false };
+  }
 
-   switchToEdit(pageHeader, field) {
-     const {state, navigate} = this.props.navigation;
-     navigate('EditSettingsComponent', {go_back_key: state.key, user: userInfo});
-   }
+  switchToEdit = (pageHeader, field) => {
+    const { state, navigate } = this.props.navigation;
+    navigate('EditSettings', { go_back_key: state.key, pageHeader: pageHeader, fieldValue: field });
+  }
 
-   render() {
-      var bgColor = '#DCE3F4';
-      return (
-         <View style={{backgroundColor:'#EFEFF4', flex:1}}>
-            <View style={{borderBottomWidth:1, backgroundColor: '#f7f7f8', borderColor:'#c8c7cc'}}>
-	       <Text style={{alignSelf:'center', marginTop:50, marginBottom:10, fontWeight:'bold', fontSize: 16}}>Profile Settings</Text>
-	    </View>
-	    <View style={{backgroundColor:'#EFEFF4', flex:1}}>
-            <SettingsList borderColor='#c8c7cc' defaultItemSize={50}>
-              <SettingsList.Header headerStyle={{marginTop:15}}/>
-	          <SettingsList.Item titleInfo = '22' hasNavArrow={true} title='Age'
-			         onPress={() => this.switchToEdit('Age', '22')/>
-	          <SettingsList.Item titleInfo = 'Male' hasNavArrow={true} title='Gender'/>
-      		  <SettingsList.Item titleInfo = 'Male, Female' hasNavArrow={true} title='Interested In (Gender)'/>
-            <SettingsList.Item titleInfo = '20-25' hasNavArrow={true} title='Interested In (Age)'/>
-      		  <SettingsList.Item titleInfo = '25 miles' hasNavArrow={true} title='Distance'/>
-      		  <SettingsList.Item title = 'Filtered Words'
-			         onPress={() => Alert.alert('Routing to FilteringWords page')}/>
-               </SettingsList>
-            </View>
+  render() {
+    var bgColor = '#DCE3F4';
+    return (
+      <View style={{ backgroundColor: '#EFEFF4', flex: 1 }}>
+        <View style={{ borderBottomWidth: 1, backgroundColor: '#f7f7f8', borderColor: '#c8c7cc' }}>
+          <Text style={{ alignSelf: 'center', marginTop: 50, marginBottom: 10, fontWeight: 'bold', fontSize: 16 }}>Profile Settings</Text>
         </View>
-      );
-   }
+        <View style={{ backgroundColor: '#EFEFF4', flex: 1 }}>
+          <SettingsList borderColor='#c8c7cc' defaultItemSize={50}>
+            <SettingsList.Header headerStyle={{ marginTop: 15 }} />
+            <SettingsList.Item titleInfo='22' hasNavArrow={true} title='Age'
+              onPress={() => this.switchToEdit('Age', '22')} />
+            <SettingsList.Item titleInfo='Male' hasNavArrow={true} title='Gender'
+              onPress={() => this.switchToEdit('Gender', 'Male')} />
+            <SettingsList.Item titleInfo='Male, Female' hasNavArrow={true} title='Interested In (Gender)'
+              onPress={() => this.switchToEdit('Interested Gender', 'Male, Female')}  />
+            <SettingsList.Item titleInfo='20-25' hasNavArrow={true} title='Interested In (Age)'
+              onPress={() => this.switchToEdit('Interested Age', '20-25')}  />
+            <SettingsList.Item titleInfo='25 miles' hasNavArrow={true} title='Distance'
+              onPress={() => this.switchToEdit('Distance', '25 miles')}  />
+            <SettingsList.Item title='Filtered Words'
+              onPress={() => this.switchToEdit('Filtered Words', ' ')}  />
+          </SettingsList>
+        </View>
+      </View>
+    );
+  }
 
-   onValueChange(value) {
-      this.setState({switchValue: value});
-   }
+  onValueChange(value) {
+    this.setState({ switchValue: value });
+  }
 }
